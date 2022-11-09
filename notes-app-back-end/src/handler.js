9a33b93a2572309342297404056265a985522507
fauziahmad37 <fauzi.enginer@gmail.com>
2022-11-09 09:30:56 +0700

    create note handler membuat request dan response status

--- notes-app-back-end/src/handler.js
+++ notes-app-back-end/src/handler.js
@@ -1,5 +1,40 @@
+const { nanoId } = require('nanoid');
+const notes = require('./notes');
+
 const addNoteHandler = (request, h) => {
     const { title, tags, body } = request.payload;
+
+    const id = nanoId(16);
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const newNote = {
+        title, tags, body, id, createdAt, updatedAt
+    };
+
+    notes.push(newNote);
+
+    const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+    if(isSuccess){
+        const response = h.response({
+            status: 'success',
+            message: 'catatan berhasil di tambahkan',
+            data: {
+                noteId: id,
+            }
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'catatan gagal di tambahkan'
+    });
+
+    response.code(500);
+    return response;
 };
 
 module.exports = { addNoteHandler };
